045da618c2901e92857833cca2d169bdcb7876a9
Ashik-Faysal <ashikfaysal.rc@gmail.com>
2023-05-24 17:06:23 +0600

    added menu item

--- src/pages/Home/Category/Category.jsx
+++ src/pages/Home/Category/Category.jsx
@@ -7,89 +7,97 @@ import slide2 from "../../../assets/home/slide2.jpg";
 import slide3 from "../../../assets/home/slide3.jpg";
 import slide4 from "../../../assets/home/slide4.jpg";
 import slide5 from "../../../assets/home/slide5.jpg";
+import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 const Category = () => {
   return (
-    <Swiper
-      // install Swiper modules
-      modules={[Pagination]}
-      spaceBetween={30}
-      slidesPerView={5}
-      navigation
-      pagination={{ clickable: true }}
-      scrollbar={{ draggable: true }}
-      onSwiper={(swiper) => console.log(swiper)}
-      onSlideChange={() => console.log("slide change")}
-    >
-      <SwiperSlide>
-        <img src={slide1} alt="" />
-        <h3
-          className="text-4xl upp
+    <section>
+          <SectionTitle
+              subHeading={"From 11.00 am to 10.00 pm"}
+              heading={"Online Order"}
+          />
+
+      <Swiper
+        // install Swiper modules
+        modules={[Pagination]}
+        spaceBetween={30}
+        slidesPerView={5}
+        navigation
+        pagination={{ clickable: true }}
+        scrollbar={{ draggable: true }}
+        onSwiper={(swiper) => console.log(swiper)}
+        onSlideChange={() => console.log("slide change")}
+      >
+        <SwiperSlide>
+          <img src={slide1} alt="" />
+          <h3
+            className="text-4xl upp
                text-center -mt-16 text-stone-500"
-        >
-          Salad
-        </h3>
-      </SwiperSlide>
-      <SwiperSlide>
-        <img src={slide2} alt="" />
-        <h3
-          className="text-4xl upp
+          >
+            Salad
+          </h3>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide2} alt="" />
+          <h3
+            className="text-4xl upp
                text-center -mt-16 text-stone-500"
-        >
-          Pizza
-        </h3>
-      </SwiperSlide>
-      <SwiperSlide>
-        <img src={slide3} alt="" />
-        <h3
-          className="text-4xl upp
+          >
+            Pizza
+          </h3>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide3} alt="" />
+          <h3
+            className="text-4xl upp
                text-center -mt-16 text-stone-500"
-        >
-          Soups
-        </h3>
-      </SwiperSlide>
-      <SwiperSlide>
-        <img src={slide4} alt="" />
-        <h3
-          className="text-4xl upp
+          >
+            Soups
+          </h3>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide4} alt="" />
+          <h3
+            className="text-4xl upp
                text-center -mt-16 text-stone-500"
-        >
-          Burger
-        </h3>
-      </SwiperSlide>
-      <SwiperSlide>
-        <img src={slide5} alt="" />
-      </SwiperSlide>
-      <SwiperSlide>
-        <img src={slide1} alt="" />
-      </SwiperSlide>
-      <SwiperSlide>
-        <img src={slide2} alt="" />
-      </SwiperSlide>
-      <SwiperSlide>
-        <img src={slide3} alt="" />
-      </SwiperSlide>
-      <SwiperSlide>
-        <img src={slide4} alt="" />
-      </SwiperSlide>
-      <SwiperSlide>
-        <img src={slide5} alt="" />
-      </SwiperSlide>
-      <SwiperSlide>
-        <img src={slide1} alt="" />
-      </SwiperSlide>
-      <SwiperSlide>
-        <img src={slide2} alt="" />
-      </SwiperSlide>
-      <SwiperSlide>
-        <img src={slide3} alt="" />
-      </SwiperSlide>
-      <SwiperSlide>
-        <img src={slide4} alt="" />
-      </SwiperSlide>
-      <SwiperSlide>
-        <img src={slide5} alt="" />
-      </SwiperSlide>
-    </Swiper>
+          >
+            Burger
+          </h3>
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide5} alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide1} alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide2} alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide3} alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide4} alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide5} alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide1} alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide2} alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide3} alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide4} alt="" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={slide5} alt="" />
+        </SwiperSlide>
+      </Swiper>
+    </section>
   );
 };
 
