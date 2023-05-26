65cdef78e8b2cd783650c6bc0b787f59e48510d5
Ashik-Faysal <ashikfaysal.rc@gmail.com>
2023-05-26 15:14:06 +0600

    added food cardd and cover

---
+++ src/pages/Order/Order/Order.jsx
@@ -0,0 +1,55 @@
+import { Helmet } from "react-helmet-async";
+import orderCover from "../../../assets/shop/banner2.jpg";
+import Cover from "../../Shared/Cover/Cover";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
+import { useState } from "react";
+import useMenu from "../../../hooks/useMenu";
+import OrderTab from "../OrderTab/OrderTab";
+import { useParams } from "react-router-dom";
+
+const Order = () => {
+    const [tabIndex, setTabindex] = useState(0);
+  const [menu] = useMenu();
+  const { category } = useParams();
+  console.log(category);
+     const desserts = menu.filter((item) => item.category === "dessert");
+     const soup = menu.filter((item) => item.category === "soup");
+     const salad = menu.filter((item) => item.category === "salad");
+     const pizza = menu.filter((item) => item.category === "pizza");
+     const drinks = menu.filter((item) => item.category === "drinks");
+  return (
+    <div>
+      <Helmet>
+        <title>Bistro Boss | Order</title>
+      </Helmet>
+      <Cover img={orderCover} title="Order Food" />
+      <Tabs defaultIndex={tabIndex} onSelect={(index) => setTabindex(index)}>
+        <TabList>
+          <Tab>Salad</Tab>
+          <Tab>Pizza</Tab>
+          <Tab>Soup</Tab>
+          <Tab>Desert</Tab>
+          <Tab>Drinks</Tab>
+        </TabList>
+        <TabPanel>
+          <OrderTab items={salad} />
+        </TabPanel>
+        <TabPanel>
+          <OrderTab items={pizza} />
+        </TabPanel>
+        <TabPanel>
+          <OrderTab items={soup} />
+        </TabPanel>
+        <TabPanel>
+          <OrderTab items={desserts} />
+        </TabPanel>
+        <TabPanel>
+          <OrderTab items={drinks} />
+        </TabPanel>
+      </Tabs>
+    </div>
+  );
+};
+
+export default Order;
--- src/pages/Shared/NavBar/NavBar.jsx
+++ src/pages/Shared/NavBar/NavBar.jsx
@@ -7,6 +7,7 @@ const NavBar = () => {
         <li>
           <Link to="/">Home</Link>
           <Link to="/menu">Our Menu</Link>
+          <Link to="/order">Order Food</Link>
         </li>
       </>
     );
